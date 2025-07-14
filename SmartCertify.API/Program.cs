8d0fd64389df001e2401813c7cb019df95145c60
Shashank Mishra <shashankmis808@gmail.com>
2025-07-14 22:18:37 +0530

    Model and Db Setup

--- SmartCertify.API/Program.cs
+++ SmartCertify.API/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SmartCertify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Register DbContext with connection string from configuration
+builder.Services.AddDbContext<SmartCertifyContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DbContext"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure()
+    )
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
